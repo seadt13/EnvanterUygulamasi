647bff04d9d855aeeb227884164ffe0b4210d3d2
seadt <seadtaydin@gmail.com>
2023-08-16 17:15:12 +0300

    first commit

---
+++ EnvanterUygulaması/Program.cs
@@ -0,0 +1,37 @@
+using EnvanterUygulamasý.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Veritabaný baðlantý ayarýný al
+var configuration = builder.Configuration;
+
+// DbContext yapýlandýrmasýný ekle
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    var connectionString = configuration.GetConnectionString("MyDbContext");
+    options.UseSqlServer(connectionString);
+});
+
+
+// Diðer hizmetleri ekle
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
